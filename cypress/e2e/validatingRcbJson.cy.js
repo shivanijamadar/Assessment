2bdba33b95ea44e39f62169b40745c4adecc230f
shivanijamadar <jamadarshivani25@gmail.com>
2023-05-31 14:54:57 +0530

    validateJsonChanges

--- cypress/e2e/validatingRcbJson.cy.js
+++ cypress/e2e/validatingRcbJson.cy.js
@@ -1,60 +1,56 @@
 describe('Common Utility to read the data from the JSON File and validate results', () => {
     it('Validate the team has max 4 foreign players', () => {
         cy.fixture('rcb').then((results)  => {
-            var players = [];
-            for(var i=0;i<results.player.length;i++){
-                if(results.player[i].country != 'India'){
-                    players.push(results.player[i].country)
+            var foreignPlayers = [];
+            results.player.forEach((players) => {
+                if(players.country != 'India'){
+                    foreignPlayers.push(players.country)
                 }
-            }
-            expect(players.length).to.equal(4);
+            });
+            expect(foreignPlayers.length).to.equal(4);
         });
     })
 
     it('Validate that there is at least one all-rounder in the team', () => {
         cy.fixture('rcb').then((results)  => {
-            var players = [];
-            for(var i=0;i<results.player.length;i++){
-                if(results.player[i].role == 'AllRounder'){
-                    players.push(results.player[i].name)
+            var allRounders = [];
+            results.player.forEach((players) => {
+                if(players.role == 'AllRounder'){
+                    allRounders.push(players.name)
                 }
-            }
-            expect(players.length).to.be.gte(1);
+            });
+            expect(allRounders.length).to.be.gte(1);
         });
     })
 
     it('Validate that there is at least one wicket keeper', () => {
         cy.fixture('rcb').then((results)  => {
-            var players = [];
-            for(var i=0;i<results.player.length;i++){
-                if(results.player[i].role == 'Wicketkeeper'){
-                    players.push(results.player[i].name)
+            var wicketKeeper = [];
+            results.player.forEach((players) => {
+                if(players.role == 'Wicketkeeper'){
+                    wicketKeeper.push(players.name)
                 }
-            }
-            expect(players.length).to.be.gte(1);
+            });
+            expect(wicketKeeper.length).to.be.gte(1);
         });
     })
 
     it('Validate Playing No: 11 and Substitute: 4 ', () => {
         cy.fixture('rcb').then((results)  => {
             var playingPlayers = [], substitutes = [];
-            for(var i=0;i<results.player.length;i++){
-                if(results.player[i].playing == 'Sub'){
-                    substitutes.push(results.player[i].name)
+            results.player.forEach((players) => {
+                if(players.playing == 'Sub'){
+                    substitutes.push(players.name)
                 }
-            }
+            });
             expect(substitutes.length).to.equal(4);
 
-            for(var i=0;i<results.player.length;i++){
-                if(results.player[i].playing == 'XI'){
-                    playingPlayers.push(results.player[i].name)
+            results.player.forEach((players) => {
+                if(players.playing == 'XI'){
+                    playingPlayers.push(players.name)
                 }
-            }
+            });
             expect(playingPlayers.length).to.equal(11);
         });
-    })
-
-    it.skip('Validate Playing No: 11 and Substitute: 4 ', () => {
-        cy.request('http://hapi.fhir.org/baseR4/Patient?_pretty=true');
     })
     })
